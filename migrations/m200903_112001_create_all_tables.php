57d1c8bda193e0d011a3ff330751ed4d59cfcecd
paskuale75 <pellicaipasquale@gmail.com>
2020-09-03 22:08:18 +0200

    one migration for all tables

---
+++ migrations/m200903_112001_create_all_tables.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace paskuale75\anagrafica\migrations;
+
+use Yii;
+use yii\db\Migration;
+
+class m200903_112001_create_all_tables extends Migration
+{
+    public function up()
+    {
+        $sql = file_get_contents(__DIR__ . '/sql_files//full.sql');
+        $command = Yii::$app->db->createCommand($sql);
+        $command->execute();
+
+        // Make sure, we fetch all errors
+        while ($command->pdoStatement->nextRowSet()) {}
+    }
+
+
+    public function down()
+    {
+    }
+}
