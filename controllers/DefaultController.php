1eb9330262ed7a4cfe73c918b3197838afc2ed12
pasquale75 <pellicanipasquale@gmail.com>
2020-09-10 17:02:57 +0200

    actionCapsList added

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -5,6 +5,7 @@ namespace paskuale75\comuni\controllers;
 
 use Faker\Provider\DateTime;
 use paskuale75\comuni\models\Citta;
+use paskuale75\comuni\models\MultiCap;
 use Yii;
 use yii\db\Query;
 use yii\helpers\Json;
@@ -35,6 +36,28 @@ class DefaultController extends Controller
             $out[] = [
                 'id'=>$d['istat'],
                 'value' => $d['comune'].' ('.$d['provincia'].')'
+            ];
+        }
+        echo Json::encode($out);
+    }
+
+
+
+    public function actionCapsList($q = null) {
+        $query = new Query;
+        $tableName = MultiCap::tableName();
+
+        $query->select('istat, cap')
+            ->from($tableName)
+            ->where(['istat' => $q])
+            ->orderBy('id');
+        $command = $query->createCommand();
+        $data = $command->queryAll();
+        $out = [];
+        foreach ($data as $d) {
+            $out[] = [
+                'id'=>$d['istat'],
+                'value' => $d['cap']
             ];
         }
         echo Json::encode($out);
